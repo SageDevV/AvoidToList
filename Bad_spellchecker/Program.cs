042fad491f881c9d241b0eced4ec38e18f5bfa00
SageDevV <pandredbz@gmail.com>
2022-04-13 09:53:46 -0300

    first commit

---
+++ Bad_spellchecker/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    private static IEnumerable<string> dictionary = null;
+    private static IEnumerable<string> story = null;
+    private static string _dictionaryWay;
+    private static string _storyWay;
+    private static IEnumerable<string> ReadFile(string file)
+    {
+        using (StreamReader reader = File.OpenText(file))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                yield return line;
+            }
+        }
+    } 
+    private static void SpellCheck()
+    {
+        var result = 
+            from word in story
+                let lowerCase = word.ToLower()
+                select new { Word = word, Ok = dictionary.ToList().Contains(lowerCase) };
+        foreach (var item in result)    
+        {
+            Console.ForegroundColor = item.Ok ? ConsoleColor.Green : ConsoleColor.Red;
+            Console.WriteLine(item.Word);
+            Console.WriteLine(" ");
+        }
+        Console.ResetColor();
+            
+    }
+    static void Main(string[] args)
+    {
+        dictionary = ReadFile(_dictionaryWay);
+        story = ReadFile(_storyWay);
+        SpellCheck();
+    }
+}
---
+++ Good_spellchecker/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    private static List<string> dictionary = new List<string>(150000);
+    private static IEnumerable<string> story = null;
+    private static string _dictionaryWay;
+    private static string _storyWay;
+    private static IEnumerable<string> ReadFile(string file)
+    {
+        using (StreamReader reader = File.OpenText(file))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                yield return line;
+            }
+        }
+    }
+    private static void SpellCheck()
+    {
+        var result =
+            from word in story
+            let lowerCase = word.ToLower()
+            select new { Word = word, Ok = dictionary.Contains(lowerCase) };
+        foreach (var item in result)
+        {
+            Console.ForegroundColor = item.Ok ? ConsoleColor.Green : ConsoleColor.Red;
+            Console.WriteLine(item.Word);
+            Console.WriteLine(" ");
+        }
+        Console.ResetColor();
+
+    }
+    static void Main(string[] args)
+    {
+        foreach (var word in ReadFile(_dictionaryWay))
+        {
+            dictionary.Add(word);
+        }
+        story = ReadFile(_storyWay);
+        SpellCheck();
+    }
+}
